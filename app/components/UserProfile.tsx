c23be9510f40c9a0ce86cc5fa1d252b63a7c0cf5
lucy schofield <scholucy@bu.edu>
2025-04-23 18:59:56 -0400

    fixed ui for userprofile image in top corner

--- app/components/UserProfile.tsx
+++ app/components/UserProfile.tsx
@@ -3,6 +3,7 @@
 import { useSession } from 'next-auth/react';
 import SignInButton from "./SignInButton";
 import SignOutButton from "./SignOutButton";
+import {Avatar, Box, Typography} from "@mui/material";
 
 export default function UserProfile() {
     const { data: session, status } = useSession();
@@ -20,10 +21,40 @@ export default function UserProfile() {
     }
 
     return (
+
         <div>
-            <p>Welcome, {session.user?.name}</p>
-            <img src={session.user?.image || ''} alt="User Avatar" />
-            <SignOutButton />
+            <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                sx={{
+                    backgroundColor: "#ffffffcc",
+                    borderRadius: "12px",
+                    padding: "0.75rem 1rem",
+                    boxShadow: 1,
+                    position: "absolute",
+                    right: "1rem",
+                    top: "0.5rem",
+                }}
+            >
+            <Avatar
+                alt={session.user?.name || ""}
+                src={session.user?.image || ""}
+                sx={{ width: 48, height: 48, mb: 1 }}
+            />
+            <Typography
+                sx={{
+                    fontFamily: "Fraunces",
+                    fontSize: "0.9rem",
+                    textAlign: "center",
+                    color: "#205781",
+                    mb: 1,
+                }}
+            >
+                {session.user?.name}
+            </Typography>
+                <SignOutButton/>
+            </Box>
         </div>
     );
 }
